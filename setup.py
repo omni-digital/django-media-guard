105c5539e0a75bb385a4738fe0720bc4f0bc4958
Jim Hill <jim@dammitjim.co.uk>
2018-11-27 16:14:58 +0000

    Moving to pipenv, pytest, pre-commit etc.

--- setup.py
+++ setup.py
@@ -1,64 +1,33 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from __future__ import unicode_literals
-
-import io
-import os
-import re
-
 from setuptools import setup, find_packages
-
-
-def read(*names, **kwargs):
-    with io.open(
-        os.path.join(os.path.dirname(__file__), *names),
-        encoding=kwargs.get('encoding', 'utf8')
-    ) as fp:
-        return fp.read()
-
-
-def find_version(*file_paths):
-    version_file = read(*file_paths)
-    version_match = re.search(
-        r"^__version__ = ['\"]([^'\"]*)['\"]",
-        version_file, re.M
-    )
-    if version_match:
-        return version_match.group(1)
-    raise RuntimeError("Unable to find version string.")
-
-
-version = find_version('media_guard', '__init__.py')
 
 setup(
     name='django-media-guard',
-    version=version,
+    version='0.1.0',
     description="""""",
-    author='Philip Tunnard-Gardner',
-    author_email='philip.gardner@omni-digital.co.uk',
-    url='https://github.com/omni-digital/assert-html',
+    author='Omni Digital',
+    author_email='engineering@omni-digital.co.uk',
+    url='https://github.com/omni-digital/django-media-guard',
     packages=find_packages(exclude=['example', 'tests']),
     include_package_data=True,
-    install_requires=[],
+    install_requires=[
+        "Django>=1.11,<2.2",
+    ],
     license="MIT",
     zip_safe=False,
     keywords=['django', 'media', 'guard', 'protect'],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Framework :: Django',
-        'Framework :: Django :: 1.10',
         'Framework :: Django :: 1.11',
         'Framework :: Django :: 2.0',
+        'Framework :: Django :: 2.1',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
 )
