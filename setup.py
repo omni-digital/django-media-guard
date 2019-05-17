486838c7dc3bd1a0a34a48b6558f0f033ba8808c
Enric Fortin <39985243+enfo14@users.noreply.github.com>
2019-05-17 15:06:24 +0100

    Bump up version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="django-media-guard",
-    version="0.1.0",
+    version="0.1.1",
     description="""""",
     author="Omni Digital",
     author_email="engineering@omni-digital.co.uk",
